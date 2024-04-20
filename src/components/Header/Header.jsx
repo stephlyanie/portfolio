a305acf7734a6cec4163b4795fa6d679034d53a5
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-04-20 14:34:32 -0700

    Set up base header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
+// Styles
+import "./Header.scss";
+
 import MenuDrawer from "../Menus/MenuDrawer";
 
 function Header() {
     return (
         <header className="site-header">
+            {/* Logo */}
+            <div className="site-header__logo"></div>
             {/* Drawer Nav Component */}
             <section className="site-header__nav">
                 <MenuDrawer />
