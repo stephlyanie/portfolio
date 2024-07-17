e220da21190dab87ed20e2273bf7930e4720c7a9
Stephanie Hall <stephanie.anie.hall@gmail.com>
2024-07-17 10:23:51 -0700

    Create burger menu for mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,43 @@
 import "./Header.scss";
 
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import MainMenu from "../Menus/MainMenu";
+import MenuDrawer from "../Menus/MenuDrawer";
 
 function Header() {
+
+  function isMobileWidth() {
+    return window.innerWidth < 767;
+  }
+
+  const [isMobileScreen, setIsMobileScreen] = useState(isMobileWidth());
+  const [headerNav, setHeaderNav] = useState();
+
+  
+    function handleWindowResize() {
+      setIsMobileScreen(isMobileWidth());
+    }
+
+  useEffect(() => {
+
+    if (isMobileWidth()) {
+      setHeaderNav(<MenuDrawer className="site-header__menu" />)
+    } else {
+      setHeaderNav(<MainMenu className="site-header__menu" />)
+    } 
+  }, [isMobileScreen])
+
+  window.addEventListener('resize', handleWindowResize);
+
+
   return (
     <header className="site-header">
       <Link to={`/`} className="site-header__logo">
         <h1>Stephanie Hall</h1>
       </Link>
+      {headerNav}
     </header>
   );
 }
--- src/components/Menus/MenuDrawer.jsx
+++ src/components/Menus/MenuDrawer.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 
 // MUI
 import { Button } from "@mui/base/Button";
-import Drawer from '@mui/joy/Drawer';
+import Drawer from "@mui/joy/Drawer";
 import List from "@mui/joy/List";
 import ListItem from "@mui/joy/ListItem";
 import ListItemDecorator from "@mui/joy/ListItemDecorator";
@@ -17,59 +17,77 @@ import ListItemButton from "@mui/joy/ListItemButton";
 import ModalClose from "@mui/joy/ModalClose";
 
 // Icons
-import { ReactComponent as Burger } from "../../assets/icons/icon-menu.svg";
+import AutoAwesome from "@mui/icons-material/AutoAwesome";
+import Cottage from "@mui/icons-material/Cottage";
+import Menu from "@mui/icons-material/Menu";
+import SentimentVerySatisfied from "@mui/icons-material/SentimentVerySatisfied";
 
 function MenuDrawer() {
-    const [open, setOpen] = useState(false);
-    const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const mainNav = [
-        {
-          name: "Portfolio",
-          icon: <Burger className="menu__nav-item-icon" />,
-          url: "/portfolio",
-        },
-        {
-          name: "Bio",
-          icon: <Burger className="menu__nav-item-icon" />,
-          url: "/bio",
-        }
-    ]
+  const mainNav = [
+    {
+      name: "Home",
+      icon: <Cottage className="menu__nav-item-icon" sx={{fill: "#6a6282"}} />,
+      url: "/",
+    },
+    {
+      name: "Projects",
+      icon: <AutoAwesome className="menu__nav-item-icon" sx={{fill: "#6a6282"}} />,
+      url: "/projects",
+    },
+    {
+      name: "Bio",
+      icon: <SentimentVerySatisfied className="menu__nav-item-icon" sx={{fill: "#6a6282"}} />,
+      url: "/bio",
+    },
+  ];
 
-    return (
-        <section className="menu">
-            <Button variant="outlined" color="neutral" onClick={() => setOpen(true)}>
-                <Burger className="menu__icon" />
-            </Button>
-            <Drawer
-                className="menu__drawer"
-                open={open}
-                onClose={() => setOpen(false)}
-            >
-                <ModalClose />
-                <div className="menu__nav">
-                    <List component="nav" className="menu__nav-list">
-                        {mainNav.map((nav) => (
-                            <ListItem key={nav.name} className="menu__nav-list-item">
-                                <ListItemButton
-                                    sx={{
-                                        padding: "1rem 0.75rem",
-                                    }}
-                                    onClick={() => {
-                                        navigate(`${nav.url}`);
-                                        setOpen(false);
-                                    }}
-                                >
-                                <ListItemDecorator>{nav.icon}</ListItemDecorator>
-                                <p className="menu__nav-item-text">{nav.name}</p>
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                </div>
-            </Drawer>
-        </section>
-    )
-};
+  return (
+    <nav className="menu-drawer">
+      <Button
+        onClick={() => setOpen(true)}
+        className="menu-drawer__open-nav-button"
+      >
+        <Menu
+          className="menu-drawer__icon"
+          sx={{
+            height: "2rem",
+            width: "2rem",
+            fill: "#fdfdff",
+          }}
+        />
+      </Button>
+      <Drawer
+        className="menu-drawer__drawer"
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <ModalClose />
+        <div className="menu-drawer__nav">
+          <List component="nav" className="menu-drawer__nav-list">
+            {mainNav.map((nav) => (
+              <ListItem key={nav.name} className="menu-drawer__nav-list-item">
+                <ListItemButton
+                  sx={{
+                    padding: "1rem 0.75rem",
+                  }}
+                  onClick={() => {
+                    navigate(`${nav.url}`);
+                    setOpen(false);
+                  }}
+                >
+                  <ListItemDecorator>{nav.icon}</ListItemDecorator>
+                  <p className="menu-drawer__nav-item-text">{nav.name}</p>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+    </nav>
+  );
+}
 
 export default MenuDrawer;
